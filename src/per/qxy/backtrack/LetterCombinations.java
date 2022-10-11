9ba11523f75a91b8ef655c9d82ca65755f5b4564
quxiangyang <quxiangyang2018@gmail.com>
2022-10-11 18:35:24 +0800

    增加leetcode17

---
+++ src/per/qxy/backtrack/LetterCombinations.java
@@ -0,0 +1,52 @@
+package per.qxy.backtrack;
+
+import java.nio.file.Path;
+import java.util.*;
+
+/*
+ * @program: CodeCaprice
+ * @description: leetCode 17
+ * @author: quxiangyang
+ * @create: 2022-10-11
+ **/
+public class LetterCombinations {
+    static String[] letterMap = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+
+    public static void main(String[] args) {
+        List<String> strings = letterCombinations("234");
+        strings.forEach(System.out::println);
+
+    }
+
+    static List<String> letterCombinations(String digits){
+        LinkedList<Character> path = new LinkedList<>();
+        List<String> result = new ArrayList<>();
+        backtracking(digits, 0, path, result);
+        return result;
+    }
+
+    static void backtracking(String digits, Integer startIndex, LinkedList<Character> path, List<String> result) {
+        if (path.size() == digits.length()) {
+            StringBuilder builder = new StringBuilder();
+            for (Character character : path) {
+                builder.append(character);
+            }
+            result.add(builder.toString());
+            return;
+        }
+
+        //首先找到要被遍历的元素集合
+        int digit = digits.charAt(startIndex) - '0';
+        String letters = letterMap[digit];
+
+        //对字符集进行遍历
+        for (int i = 0; i < letters.length() ; i++) {
+            path.add(letters.charAt(i));
+            backtracking(digits, startIndex + 1, path, result);
+            path.removeLast();
+        }
+    }
+
+
+}
